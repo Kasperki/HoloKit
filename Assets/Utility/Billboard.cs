ab8022d37c749300fa2e76e5a2f604541ebfa13e
Kasper Kiiskinen <kisperkkk@hotmail.com>
2017-04-07 11:24:01 +0300

    Billboards now in every X Y Z axis.

--- Assets/Utility/Billboard.cs
+++ Assets/Utility/Billboard.cs
@@ -8,7 +8,8 @@ namespace HoloKit
     {
         XYZ,
         X,
-        Y
+        Y,
+        Z
     }
 
     public class Billboard : MonoBehaviour
@@ -21,22 +22,13 @@ namespace HoloKit
         public Quaternion DefaultRotation { get; private set; }
 
         public bool Smoothed = true;
+        public float SmoothingSpeed = 2.0f;
 
-        /// <summary>
-        /// Duration after enable to face the camera
-        /// </summary>
-        public float Duration = 2.0f;
-
-        private float startTime, t;
-        private Vector3 startForward, startUp;
+        private Vector3 rot;
 
         private void OnEnable()
         {
             DefaultRotation = gameObject.transform.rotation;
-
-            startForward = transform.forward;
-            startUp = transform.up;
-            startTime = Time.time;
         }
 
         /// <summary>
@@ -44,33 +36,29 @@ namespace HoloKit
         /// </summary>
         private void Update()
         {
-            Vector3 forward;
-            Vector3 up;
-
-            t = Smoothed ? Mathf.SmoothStep(0, 1, (Time.time - startTime) / Duration) : 1;
-
             switch (PivotAxis)
             {
                 case PivotAxis.X:
-                    Vector3 right = transform.right; // Fixed right
-                    forward = Vector3.ProjectOnPlane(Camera.main.transform.forward, right).normalized;
-                    up = Vector3.Lerp(startUp, Vector3.Cross(forward, right), t);
+                    rot = new Vector3(Camera.main.transform.rotation.eulerAngles.x, 0, 0);
                     break;
 
                 case PivotAxis.Y:
-                    up = transform.up; // Fixed up
-                    forward = Vector3.Lerp(startForward, Camera.main.transform.forward, t);
+                    rot = new Vector3(0, Camera.main.transform.rotation.eulerAngles.y, 0);
+                    break;
+
+                case PivotAxis.Z:
+                    rot = new Vector3(0, 0, Camera.main.transform.rotation.eulerAngles.z);
                     break;
 
                 case PivotAxis.XYZ:
                 default:
-                    forward = Vector3.Lerp(startForward, Camera.main.transform.forward, t);
-                    up = Vector3.Lerp(startUp, Camera.main.transform.up, t);
+                    rot = Camera.main.transform.rotation.eulerAngles;
                     break;
             }
 
-            // Calculate and apply the rotation required to reorient the object
-            transform.rotation = Quaternion.LookRotation(forward, up);
+            var t = Smoothed ? Time.deltaTime * SmoothingSpeed : 1;
+            var targetRotation = Quaternion.Euler(rot);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, t);
         }
     }
 }
